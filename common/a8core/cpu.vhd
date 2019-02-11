22e0c416c9996992c0f02c5a4179964e927eb717
sorgelig <pour.garbage@gmail.com>
2019-02-12 04:38:49 +0800

    Back to old CPU.

--- common/a8core/cpu.vhd
+++ common/a8core/cpu.vhd
@@ -28,39 +28,87 @@ PORT
 END cpu;
 
 architecture vhdl of cpu is
-	signal CPU_ENABLE: std_logic; -- Apply Antic HALT and throttle
+	signal cpu_enable: std_logic; -- Apply Antic HALT and throttle
+
+	-- Support for Peter's core (NMI patch applied)
+	signal we : std_logic;
+	signal nmi_pending_next : std_logic; -- NMI during RDY
+	signal nmi_pending_reg : std_logic;
+	signal nmi_n_adjusted : std_logic;
+	signal nmi_n_reg : std_logic;
+	signal nmi_edge : std_logic;
+
 	signal addr : std_logic_vector(23 downto 0);
 	signal cpu_do : std_logic_vector(7 downto 0);
 	signal cpu_di : std_logic_vector(7 downto 0);
 	signal cpu_rwn : std_logic;
-	
 BEGIN
-	CPU_ENABLE <= ENABLE and MEMORY_READY and THROTTLE;
-	DO <= cpu_do;
-	R_W_n <= cpu_rwn;
-
-	cpu_di <= cpu_do when cpu_rwn='0' else DI;
+	cpu_enable <= ENABLE and MEMORY_READY and THROTTLE;
 	
-	cpu : work.T65
-	port map
+	-- CPU designed by Peter W - as used in Chameleon
+	cpu_6502_peter: work.cpu_65xx
+	generic map
 	(
-		Mode  => "00",
-		Res_n => not RESET,
-		Enable => CPU_ENABLE,
-		Clk => CLK,
-		Rdy => RDY,
-		Abort_n => '1',
-		IRQ_n => IRQ_n,
-		NMI_n => NMI_n,
-		SO_n => '1',
-		R_W_n => cpu_rwn,
-		A => addr,
-		DI => cpu_di,
-		DO => cpu_do
-	);	
-	
-	A <= addr(15 downto 0);
-		
+		pipelineOpcode => false,
+		pipelineAluMux => false,
+		pipelineAluOut => false
+	)
+	port map (
+		clk => clk,
+		enable => (cpu_enable and (RDY or we)) or reset,
+		halt => '0',
+		reset=>reset,
+		nmi_n=>nmi_n_adjusted,
+		irq_n=>irq_n,
+		d=>unsigned(di),
+		std_logic_vector(q)=>do,
+		std_logic_vector(addr)=>a,
+		WE=>we
+	);
+
+	nmi_edge <= not(nmi_n) and nmi_n_reg;
+	nmi_pending_next <= (nmi_edge and not(RDY or we)) or (nmi_pending_reg and not(RDY)) or (nmi_pending_reg and RDY and not(cpu_enable));
+	nmi_n_adjusted <= not(nmi_pending_reg) and nmi_n;
+
+	-- register
+	process(clk,reset)
+	begin
+		if (RESET = '1') then
+			nmi_pending_reg <= '0';
+			nmi_n_reg <= '1';
+		elsif rising_edge(clk) then
+			nmi_pending_reg <= nmi_pending_next;
+			nmi_n_reg <= nmi_n;
+		end if;
+	end process;	
+
+	-- outputs
+	r_w_n <= not(we);
 	CPU_FETCH <= ENABLE and THROTTLE;
+
+--	DO <= cpu_do;
+--	R_W_n <= cpu_rwn;
+--
+--	cpu_di <= cpu_do when cpu_rwn='0' else DI;
+--	
+--	cpu : work.T65
+--	port map
+--	(
+--		Mode  => "00",
+--		Res_n => not RESET,
+--		Enable => cpu_enable,
+--		Clk => CLK,
+--		Rdy => RDY,
+--		Abort_n => '1',
+--		IRQ_n => IRQ_n,
+--		NMI_n => NMI_n,
+--		SO_n => '1',
+--		R_W_n => cpu_rwn,
+--		A => addr,
+--		DI => cpu_di,
+--		DO => cpu_do
+--	);	
+--
+--	A <= addr(15 downto 0);
 
 END vhdl;
