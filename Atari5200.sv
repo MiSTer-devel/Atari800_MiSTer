806b9694f6f738f6c0175db421dce8e6a3f3c8b6
Sorgelig <pour.garbage@gmail.com>
2025-07-10 21:29:12 +0800

    Update sys.

--- Atari5200.sv
+++ Atari5200.sv
@@ -58,6 +58,7 @@ module emu
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
 	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -189,6 +190,7 @@ assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
 assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 wire [1:0] ar       = status[23:22];
 wire       vcrop_en = status[24];
