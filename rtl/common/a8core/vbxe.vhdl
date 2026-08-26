934d4b2beae8dc831681fb760c62a8b0f18c54ef
Wojciech Mostowski <wojciech.mostowski@gmail.com>
2026-08-26 21:33:14 +0200

    More VBXE bettering

--- rtl/common/a8core/vbxe.vhdl
+++ rtl/common/a8core/vbxe.vhdl
@@ -161,8 +161,13 @@ signal memac_check_reg : std_logic_vector(1 downto 0);
 
 signal dma_state_reg : std_logic_vector(3 downto 0);
 signal dma_state_next : std_logic_vector(3 downto 0);
+signal dma_wait_reg : integer range 0 to cycle_length-1;
+signal dma_wait_next : integer range 0 to cycle_length-1;
+
 signal memac_request_complete_reg : std_logic;
 signal memac_request_complete_next : std_logic;
+signal memac_serviced_reg : std_logic;
+signal memac_serviced_next : std_logic;
 signal memac_request_reg : std_logic_vector(1 downto 0);
 signal memac_request_next : std_logic_vector(1 downto 0);
 signal memac_data_reg : std_logic_vector(7 downto 0);
@@ -806,8 +811,11 @@ begin
 		mems_reg <= "0UUUUUUU";
 		memb_reg <= "00UUUUUU";
 		dma_state_reg <= "1111";
+		dma_wait_reg <= 0;
+
 		memac_request_complete_reg <= '0';
 		memac_request_reg <= "00";
+		memac_serviced_reg <= '0';
 		memac_address_reg <= (others=>'0');
 		memac_check_reg <= "00";
 		memac_data_reg <= (others => '0');
@@ -894,8 +902,10 @@ begin
 		p3_reg <= p3_next;
 
 		dma_state_reg <= dma_state_next;
+		dma_wait_reg <= dma_wait_next;
 		memac_request_complete_reg <= memac_request_complete_next;
 		memac_request_reg <= memac_request_next;
+		memac_serviced_reg <= memac_serviced_next;
 		memac_address_reg <= memac_address_next;
 		memac_check_reg <= memac_check_next;
 		memac_data_reg <= memac_data_next;
@@ -1100,7 +1110,7 @@ end process;
 
 -- VBXE DMA state machine
 process(enable_179,
-	dma_state_reg, memac_request_complete_reg,vram_op_reg,memac_data_reg,memac_datain_reg,memac_request_reg, memac_check_reg,
+	dma_state_reg, dma_wait_reg, memac_request_complete_reg,vram_op_reg,memac_serviced_reg,memac_data_reg,memac_datain_reg,memac_request_reg, memac_check_reg,
 	memc_reg,mems_reg,memb_reg,vram_data_in,vram_request_complete,memac_address_reg, blitter_vram_address,blitter_vram_data,blitter_vram_wren,blitter_vram_data_in_reg,
 	blitter_status,blitter_pending_reg, xdl_ovscr_h_reg, xdl_ovscr_v_reg,
 	xdl_map_active_reg, xdl_ov_active_reg, xdl_ov_text_reg, xdl_fetch_reg, xdl_pending_reg, xdl_read_state_reg, xdl_cmd_reg, xdl_active_reg,
@@ -1123,7 +1133,9 @@ begin
 	vram_data <= (others => '0');
 	blitter_vram_data_in_next <= blitter_vram_data_in_reg;
 	dma_state_next <= dma_state_reg;
+	dma_wait_next <= dma_wait_reg + 1;
 	memac_request_complete_next <= memac_request_complete_reg;
+	memac_serviced_next <= memac_serviced_reg;
 	vram_op_next <= vram_op_reg;
 	memac_data_next <= memac_data_reg;
 	blitter_pending_next <= blitter_pending_reg;
@@ -1455,33 +1467,38 @@ begin
 			dma_state_next <= "1101";
 		end if;
 	when "1101" =>
+		memac_request_complete_next <= '0';
 		if (vram_op_reg = '0') or (vram_request_complete = '1') then
 			vram_op_next <= '0';
 			-- Additional MEMAC request check, in case the regular CPU one was pushed out by DMA
-			-- access from the previous cycle, or in case we have DMA request flood to speed up things
-			if memac_request_reg(0) = '1' then
-				vram_op_next <= '1';
-				vram_request <= '1';
-				if memac_check_reg(0) = '1' then
-					vram_addr <= mems_reg(6 downto 0) & memac_address_reg(11 downto 0);
-					case memc_reg(1 downto 0) is
-					when "00" =>
-						null;
-					when "01" =>
-						vram_addr(12) <= std_logic_vector(unsigned(memac_address_reg(15 downto 12)) - unsigned(memc_reg(7 downto 4)))(0);
-					when "10" =>
-						vram_addr(13 downto 12) <= std_logic_vector(unsigned(memac_address_reg(15 downto 12)) - unsigned(memc_reg(7 downto 4)))(1 downto 0);
-					when "11" =>
-						vram_addr(14 downto 12) <= std_logic_vector(unsigned(memac_address_reg(15 downto 12)) - unsigned(memc_reg(7 downto 4)))(2 downto 0);
-					end case;
-				else -- memac_check_b = '1'
-					vram_addr <= memb_reg(4 downto 0) & memac_address_reg(13 downto 0);
+			-- access from the previous cycle, PBI delay, or in case we have DMA request flood
+			-- to speed up things
+			if dma_wait_reg < cycle_length - 4 then
+				if memac_request_reg(0) = '1' then
+					vram_op_next <= '1';
+					vram_request <= '1';
+					memac_serviced_next <= '1';
+					if memac_check_reg(0) = '1' then
+						vram_addr <= mems_reg(6 downto 0) & memac_address_reg(11 downto 0);
+						case memc_reg(1 downto 0) is
+						when "00" =>
+							null;
+						when "01" =>
+							vram_addr(12) <= std_logic_vector(unsigned(memac_address_reg(15 downto 12)) - unsigned(memc_reg(7 downto 4)))(0);
+						when "10" =>
+							vram_addr(13 downto 12) <= std_logic_vector(unsigned(memac_address_reg(15 downto 12)) - unsigned(memc_reg(7 downto 4)))(1 downto 0);
+						when "11" =>
+							vram_addr(14 downto 12) <= std_logic_vector(unsigned(memac_address_reg(15 downto 12)) - unsigned(memc_reg(7 downto 4)))(2 downto 0);
+						end case;
+					else -- memac_check_b = '1'
+						vram_addr <= memb_reg(4 downto 0) & memac_address_reg(13 downto 0);
+					end if;
+					if memac_request_reg(1) = '1' then
+						vram_wr_en <= '1';
+						vram_data <= memac_datain_reg;
+					end if;
+					dma_state_next <= "1110";
 				end if;
-				if memac_request_reg(1) = '1' then
-					vram_wr_en <= '1';
-					vram_data <= memac_datain_reg;
-				end if;
-				dma_state_next <= "1110";
 			else
 				dma_state_next <= "1111";
 			end if;
@@ -1493,9 +1510,9 @@ begin
 				memac_data_next <= vram_data_in;
 			end if;
 			memac_request_complete_next <= '1';
-			dma_state_next <= "1111";
+			dma_state_next <= "1101";
 		end if;
-	when "1111" =>		--
+	when "1111" =>
 		memac_request_complete_next <= '0';
 	when others =>
 	end case;
@@ -1658,6 +1675,12 @@ begin
 
 	xdl_read_required := false;
 	if xdl_or_blitter_notify then
+		if memac_serviced_reg = '1' then
+			xdl_or_blitter_notify := false;
+			memac_serviced_next <= '0';
+		end if;
+	end if;
+	if xdl_or_blitter_notify then
 		if xdl_read_state_reg > 0 and xdl_read_state_reg < 24 then
 			xdl_read_state_next <= xdl_read_state_reg + 1;
 		end if;
@@ -1716,6 +1739,7 @@ begin
 
 	if enable_179 = '1' then
 		dma_state_next <= "0000";
+		dma_wait_next <= 0;
 	end if;
 end process;
 
